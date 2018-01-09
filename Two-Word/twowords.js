3cb3134193703cb9344a5792ae1a5966c9d064de
andriiglukhyi <andrigluxii@gmail.com>
2018-01-09 09:35:48 -0800

    new stuff

--- Two-Word/twowords.js
+++ Two-Word/twowords.js
@@ -1,5 +1,17 @@
-function abbrevName(name){
-    var new1 = name.replace(/[a-z]/g, '');
-    console.log(new1);
-    return new1.replace(/ /g, '.')
+'use strict';
+
+function nbYear(p0, percent, aug, p) {
+  var counter = 0;
+  while (p0 < p) {
+    p0 += p0 * percent + aug;
+    counter ++;
+  }
+  return counter;
+} var a = (1500, 5, 100, 5000);
+nbYear(a);
+
+
+function lil (){
+  console.log('hello');
 }
+lil();
