6135ac7d3a03e2ce560352b6beb0010f58298e2d
andriiglukhyi <andrigluxii@gmail.com>
2018-01-19 15:29:17 -0800

    new

--- wordstosentense/solution.js
+++ wordstosentense/solution.js
@@ -1,16 +1,3 @@
 function formatWords(words){
-    let a;
-    var arr = [];
-    if (words){  
-    for (var i=0; i<words.length; i++){
-        if (words[i].length>0){
-            arr.push(words[i]);
-        }
-    }
-    }
-    
-    if (arr[0].length === 1){a = arr[0]}
-    if (arr.length === 2){a = arr[0]+ ' and ' + arr[1]}
-    else { a = arr.join().replace(/,([^,]*)$/, ' and $1').replace(/,/g, ', ')}
-    return a
-    }
+    if (!words) return "";
+    return words.filter(function(a) { return a !== ''}).join(', ').replace(/(, )+(\S+)$/, ' and $2');
